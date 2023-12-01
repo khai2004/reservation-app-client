5aad50fb34b76dff531555e3c6a854f4c7ab2d00
Duc Khai <khaiducnguyen325@gmail.com>
2023-12-01 17:15:25 +0700

    create dynamic api url

--- src/slices/apiSlice.js
+++ src/slices/apiSlice.js
@@ -1,8 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { BASE_URL } from '../constanst';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: BASE_URL,
+  baseUrl: `${process.env.REACT_APP_API_URL}/api`,
   credentials: 'include',
 });
 
